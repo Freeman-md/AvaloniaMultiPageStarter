d431089c68f4c45e911d66b6b7c5feb0d0e748bf
Freeman-md <66797587+Freeman-md@users.noreply.github.com>
2025-07-26 12:54:58 +0100

    feat: implement SettingsViewModel and corresponding XAML for user settings

--- ViewModels/Pages/SettingsViewModel.cs
+++ ViewModels/Pages/SettingsViewModel.cs
@@ -1,3 +1,26 @@
+using AvaloniaMultiPageStarter.Interfaces;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
 namespace AvaloniaMultiPageStarter.ViewModels.Pages;
 
-public class SettingsViewModel : ViewModelBase { }
+public partial class SettingsViewModel : ViewModelBase
+{
+    private readonly IAppConfigService _config;
+
+    [ObservableProperty]
+    private string inputName;
+
+    public SettingsViewModel(IAppConfigService config)
+    {
+        _config = config;
+        InputName = _config.Username;
+    }
+
+    [RelayCommand]
+    private void SaveName()
+    {
+        if (!string.IsNullOrWhiteSpace(InputName))
+            _config.Username = InputName;
+    }
+}
